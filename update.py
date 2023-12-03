1e0998a076ee418c94f85753bddc6d3379d61ce3
Michael <michael_dube@mymail.eku.edu>
2023-12-03 14:35:13 -0500

    added ability to pull itself from github. now to see if it replaces itself.

--- update.py
+++ update.py
@@ -3,3 +3,4 @@ import urllib.request, sys
 #important comment
 
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/updatesExperiment/main/textFile.txt', 'textFile.txt')
+urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/updatesExperiment/main/update.py', 'working.py')
