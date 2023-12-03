011243959ed7508315c3cdbc0269713ff873a726
Michael <michael_dube@mymail.eku.edu>
2023-12-03 14:39:22 -0500

    added line of code to pull itself from github

--- update.py
+++ update.py
@@ -3,4 +3,3 @@ import urllib.request, sys
 #important comment
 
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/updatesExperiment/main/textFile.txt', 'textFile.txt')
-urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/updatesExperiment/main/update.py', 'working.py')
